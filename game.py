06cfd56129eea1d2ad71cebac40de239ca962cdb
stickersandstamps <ext220019@utdallas.edu>
2024-11-29 21:34:32 -0600

    Added code for self in Game class to set font and menu style preferences.

--- game.py
+++ game.py
@@ -2,3 +2,17 @@ import pygame
 
 class Game():
     def __init__(self):
+        #main menu self code
+        pygame.init()
+        self.running, self.playing = True, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.DISPLAY_W, self.DISPLAY_H = 480, 270
+        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
+        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
+        self.font_name = '8-BIT WONDER.TTF'
+        #self.font_name = pygame.font.get_default_font()
+        self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
+        self.main_menu = MainMenu(self)
+        self.options = OptionsMenu(self)
+        self.credits = CreditsMenu(self)
+        self.curr_menu = self.main_menu
