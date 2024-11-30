3272fdfd6ed59095a5f2f2acd6fb76662ea255a0
stickersandstamps <ext220019@utdallas.edu>
2024-11-29 21:46:16 -0600

    Added display_menu function to display the menu.

--- menu.py
+++ menu.py
@@ -79,3 +79,15 @@ class MainMenu(Menu):
             self.volx, self.voly = self.mid_w, self.mid_h + 20
             self.controlsx, self.controlsy = self.mid_w, self.mid_h + 40
             self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
+
+        def display_menu(self):
+            self.run_display = True
+            while self.run_display:
+                self.game.check_events()
+                self.check_input()
+                self.game.display.fill((0, 0, 0))
+                self.game.draw_text('Options', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 30)
+                self.game.draw_text("Volume", 15, self.volx, self.voly)
+                self.game.draw_text("Controls", 15, self.controlsx, self.controlsy)
+                self.draw_cursor()
+                self.blit_screen()
