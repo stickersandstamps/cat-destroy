e01a1703917e0809d755daf3519a51e7b256517f
stickersandstamps <ext220019@utdallas.edu>
2024-11-29 22:24:06 -0600

    Changed screen dimensions.

--- game.py
+++ game.py
@@ -7,15 +7,15 @@ class Game():
         pygame.init()
         self.running, self.playing = True, False
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
-        self.DISPLAY_W, self.DISPLAY_H = 480, 270
+        self.DISPLAY_W, self.DISPLAY_H = 960, 540
         self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
         self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
-        self.font_name = '8-BIT WONDER.TTF'
+        self.font_name = pygame.font.get_default_font()
         #self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
         self.main_menu = MainMenu(self)
-        self.options = OptionsMenu(self)
-        self.credits = CreditsMenu(self)
+        #self.options = OptionsMenu(self)
+        #self.credits = CreditsMenu(self)
         self.curr_menu = self.main_menu
 
     def game_loop(self):
