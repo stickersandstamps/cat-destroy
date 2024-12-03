d4fe8d1ddc277a6cf3344ff7a07e055944ab2db2
stickersandstamps <ext220019@utdallas.edu>
2024-12-02 18:45:21 -0600

    Adde code to play music.

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from pygame import mixer
 g = Game()
 
 while g.running:
-    
+    #music code
+    mixer.init() 
+    mixer.music.load("")
+    mixer.music.set_volume(0.7) 
+    mixer.music.play()
+
     g.curr_menu.display_menu()
     g.game_loop()
