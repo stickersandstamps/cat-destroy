63ab1c49aa711d56a7928eb7d91dd08d046069ac
stickersandstamps <ext220019@utdallas.edu>
2024-12-04 01:11:37 -0600

    Added three functions to project.py which use cowsay to show a cat.

--- src/project.py
+++ src/project.py
@@ -1,25 +1,31 @@
+import cowsay
+import pygame
 from game import Game
 
-g = Game()
-
-while g.running:
-    g.curr_menu.display_menu()
-    g.game_loop()
 
 def main():
-    ...
+
+    catSpeak1()
+    catSpeak2()
+    catSpeak3()
+
+    g = Game()
+
+    while g.running:
+        g.curr_menu.display_menu()
+        g.game_loop()
 
 
-def function_1():
-    ...
+def catSpeak1():
+    cowsay.kitty('Most of my code is in my files menu.py & game.py! :D')
 
 
-def function_2():
-    ...
+def catSpeak2():
+    cowsay.kitty('I used project.py mainly to just run the game. :P')
 
 
-def function_3():
-    ...
+def catSpeak3():
+    cowsay.kitty(':)')
 
 
 if __name__ == "__main__":
